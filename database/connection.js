aa17a87176f71f423ccfc79952a4ba0332282ded
Murilo Augusto Moreira <flixzs12@gmail.com>
2026-03-17 19:45:39 -0300

    Utilizando express e cors para criar um servidor web e apis

---
+++ database/connection.js
@@ -0,0 +1,18 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres'
+})
+
+
+async function connectDb(){
+    await client.connect() 
+    console.log('COnectado ao banco de dados')
+}
+
+connectDb()
+
+module.exports = client
--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-const client = require('./bot/client')
+const client = require('./database/connection')
+const readLine = require('readline')
+const express = require('express')
+const cors = require('cors')
+
+let index = 0
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true
+})
+
+rl.on('line', async (message) =>{
+    if(message == 'task'){
+        const result = await createTask()
+        console.log(result.rows[0])
+    }
+})
+
+async function createTask(){
+
+
+    const result = await client.query(
+        'INSERT INTO tasks (text) VALUES ($1) RETURNING *',
+        [index]
+    )
+    index++;
+
+    return result
+}
 
 
 
+const app = express();
+app.use(cors())
+
+app.get('/api/tasks', async (req,res) =>{
+
+    try{
+        const result = await client.query(
+            'SELECT * FROM tasks ORDER BY data_criacao DESC'
+        )
+        res.json(result.rows)
+    }catch (err){
+        res.status(500).json({ error: err.message})
+    }
 
 
-client.initialize()
+})
+
+
+app.get('/', (req, res) => {
+    res.send('API está rodando 🚀')
+})
+
+app.listen(3000, () => console.log('API rodando na porta 3000'));
